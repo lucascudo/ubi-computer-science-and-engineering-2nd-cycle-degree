0457ac6203fd7b72a3b58f15fd05dd2a5939879e
Teixeira Rocha, Lucas <lucas.teixeira.rocha@oracle.com>
2022-10-13 20:35:58 +0100

    extend test

--- ubi-assistant/cypress/e2e/spec.cy.js
+++ ubi-assistant/cypress/e2e/spec.cy.js
@@ -1,10 +1,10 @@
 describe('ubi-assistant', () => {
-  it('passes', () => {
-    const homeText = 'Frequência: 04.01.2023, 18h00, sala 6.26'
+  it('verify ngpombo\'s homepage', () => {
+    const examText = 'Frequência: 04.01.2023, 18h00, sala 6.26'
     const index = {
       row: 6,
       column: 1,
-    };
+    }
 
     cy.visit('~ngpombo/')
     cy.get('ul#navigation').children('li').eq(2).contains('Teaching').click()
@@ -12,12 +12,14 @@ describe('ubi-assistant', () => {
     cy.get('ul.ul-dates').eq(2).children('li').eq(1).children('div.content').children('p').children('a')
         .should('have.attr', 'href', './units/sq.html').invoke('removeAttr', 'target').click()
 
-    cy.get('div#News > ul').contains('li', homeText)
+    cy.get('div#News > ul').contains('li', examText).should(elem => {
+      expect(elem.text().trim()).to.equal(examText)
+    })
     cy.get('button').eq(2).contains('Lecture Notes').click()
     cy.get('div#LectureNotes > table > tbody > tr').eq(index.row).children('td').eq(index.column).should('be.empty')
     cy.get('button').eq(3).contains('Grades').click()
     cy.get('div#Grades').contains('TBD').should(elem => {
-      expect(elem.text().trim()).to.equal('TBD');
-    });
+      expect(elem.text().trim()).to.equal('TBD')
+    })
   })
 })
