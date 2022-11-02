b381ca15c5d37928ecf076600055ba0b78368bc4
Teixeira Rocha, Lucas <lucas.teixeira.rocha@oracle.com>
2022-11-02 18:06:30 +0000

    Updates 14464 test params

--- ubi-assistant/cypress.config.js
+++ ubi-assistant/cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "2a6gku",
   e2e: {
     experimentalSessionAndOrigin: true,
     setupNodeEvents(on, config) {
