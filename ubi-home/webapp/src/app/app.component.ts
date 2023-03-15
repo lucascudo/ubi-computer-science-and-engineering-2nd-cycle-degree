2a47665482bfaa2055f232279504e502528d943d
Lucas Rocha <13350752+lucascudo@users.noreply.github.com>
2023-03-15 12:27:31 +0000

    Hello UBI-HOME

---
+++ ubi-home/webapp/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  lightStatusList: Observable<any[]>;
+  lightIsOn: boolean = false;
+
+  doorStatusList: Observable<any[]>;
+  doorIsOpen: boolean = false;
+
+  constructor(private firestore: AngularFirestore) {
+    this.lightStatusList = firestore.collection('lightStatus').valueChanges();
+    this.lightStatusList.subscribe(
+      (res) => this.lightIsOn = res.sort((a, b) => a.timestamp - b.timestamp).pop().isOn);
+
+    this.doorStatusList = firestore.collection('doorStatus').valueChanges();
+    this.doorStatusList.subscribe(
+      (res) => this.doorIsOpen = res.sort((a, b) => a.timestamp - b.timestamp).pop().isOn);
+  }
+
+  sendCommand(target: string, action: string) {
+    const timestamp = new Date();
+    const commands = this.firestore.collection('commands');
+    commands.add({timestamp, target, action});
+  }
+}
---
+++ ubi-home/webapp/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatIconModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
+  ],
+  providers: [
+    { provide: FIREBASE_OPTIONS, useValue: environment.firebase }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
