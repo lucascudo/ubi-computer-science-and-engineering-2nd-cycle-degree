f1b78f6fc2d605b3487659f34bf30fd5fff328a3
Lucas Rocha <13350752+lucascudo@users.noreply.github.com>
2023-03-15 16:57:21 +0000

    Update index.js

--- ubi-home/srv/index.js
+++ ubi-home/srv/index.js
@@ -5,46 +5,57 @@ import env from "./env.js";
 initializeApp(env.firebase);
 const db = getFirestore();
 
-const commandsSnapshot = await getDocs(collection(db, "commands"));
-let lastCommand = commandsSnapshot.docs.sort(
-  (a, b) => a.data().timestamp - b.data().timestamp).pop();
-
-onSnapshot(collection(db, "commands"), async (commandsSnapshot) => {
-    const command = commandsSnapshot.docs.sort(
-      (a, b) => a.data().timestamp - b.data().timestamp).pop();
-    if (command &&  command.id !== lastCommand?.id) {
-      const commandData = command.data();
-      await runCommand(commandData.target, commandData.action);
-      lastCommand = command;
-    }
-}, err => console.error(err));
-
-async function runCommand(target, action) {
-  const timestamp = new Date();
-  const turnLight = (isOn) => addDoc(collection(db, "lightStatus"), {timestamp, isOn});
-  const moveDoor = (isOpen) => addDoc(collection(db, "doorStatus"), {timestamp, isOpen});
-
-  const commands = {
-    door: {
-      open: () => moveDoor(true),
-      close: () => moveDoor(false)
-    },
-    light: {
-      turnon: () => turnLight(true),
-      turnoff: () => turnLight(false)
-    }
-  }
-
-  console.log(`Running command: ${action} ${target}`);
-  await commands[target][action]();
-}
-
 /*
 const five = require("johnny-five");
 const  board = new five.Board();
 
 board.on("ready", () => {
-	const led = new five.Led(13);
-	led.blink(500);
+*/
+  const commandsSnapshot = await getDocs(collection(db, "commands"));
+  let lastCommand = commandsSnapshot.docs.sort(
+    (a, b) => a.data().timestamp - b.data().timestamp).pop();
+
+  onSnapshot(collection(db, "commands"), async (commandsSnapshot) => {
+      const command = commandsSnapshot.docs.sort(
+        (a, b) => a.data().timestamp - b.data().timestamp).pop();
+      if (command &&  command.id !== lastCommand?.id) {
+        const commandData = command.data();
+        lastCommand = command;
+        await runCommand(commandData.target, commandData.action);
+      }
+  }, err => console.error(err));
+
+  async function runCommand(target, action) {
+    function turnLight(isOn) {
+      /*
+      const led = new five.Led(13);
+      if (isOn) {
+        led.on();
+      } else {
+        led.off();
+      }
+      */
+      addDoc(collection(db, "lightStatus"), {timestamp, isOn});
+    }
+
+    function moveDoor(isOpen) {
+      addDoc(collection(db, "doorStatus"), {timestamp, isOpen});
+    }
+
+    const timestamp = new Date();
+    const commands = {
+      door: {
+        open: () => moveDoor(true),
+        close: () => moveDoor(false)
+      },
+      light: {
+        turnon: () => turnLight(true),
+        turnoff: () => turnLight(false)
+      }
+    }
+    console.log(`Running command: ${action} ${target}`);
+    await commands[target][action]();
+  }
+/*
 });
 */
